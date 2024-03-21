5563b1edd22732077722784265b2e389ac9c35f3
Trent <92242968+MrKingst3r@users.noreply.github.com>
2024-03-20 21:04:54 -0500

    Add files via upload

---
+++ Classic-Nails-Frontend/src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Header from '../components/Header.vue'
+import HomeSections from '../components/HomeSections.vue'
+</script>
+
+<template>
+  <Header />
+  <HomeSections />
+</template>
---
+++ Classic-Nails-Frontend/src/views/ServicesView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Header from '../components/Header.vue'
+import ServiceSections from '../components/ServiceSections.vue'
+</script>
+
+<template>
+  <Header />
+  <ServiceSections />
+</template>
