c922d197a99e0e8c1b4e4fe0ab96a12c585a0d38
Trent <92242968+MrKingst3r@users.noreply.github.com>
2024-03-20 21:01:58 -0500

    Converted Html project to vue.js

---
+++ src/components/ServiceSections.vue
@@ -0,0 +1,158 @@
+<template>
+    <section class="u-clearfix u-white u-section-1" id="carousel_c0ec">
+        <div class="u-container-style u-expanded-width u-group u-shape-rectangle u-group-1" data-animation-name="flipIn" data-animation-duration="1500" data-animation-direction="X" data-animation-delay="750">
+            <div class="u-container-layout u-container-layout-1">
+                <h2 class="u-align-center u-text u-text-default u-text-1" data-animation-name="customAnimationIn" data-animation-duration="1500">Our ​Treatments</h2>
+                <p class="u-align-center u-text u-text-2" data-animation-name="customAnimationIn" data-animation-duration="1500" data-animation-delay="500">Consectetur adipiscing elit nullam nunc justo sagittis suscipit ultrices.</p>
+            </div>
+        </div>
+        <div class="custom-expanded u-list u-list-1">
+            <div class="u-repeater u-repeater-1">
+                <div class="u-align-center u-container-align-center u-container-style u-hover-feature u-list-item u-palette-1-light-2 u-repeater-item u-shape-rectangle u-list-item-1" data-animation-direction="Up" data-animation-name="customAnimationIn" data-animation-duration="1500">
+                    <div class="u-container-layout u-similar-container u-valign-top u-container-layout-2">
+                        <h3 class="u-align-center u-text u-text-body-color u-text-3"> Face </h3>
+                        <div class="u-border-5 u-border-white u-image u-image-circle u-image-1" alt="" data-image-width="740" data-image-height="1110"></div>
+                        <p class="u-align-center u-text u-text-4"> Chemical Peels, Microneedling, Microderbabrasion &amp; Traditional Facials suscipit ultrices.</p> 
+                        <a href="https://nicepage.com/k/presentation-html-templates" class="u-active-none u-align-center u-border-1 u-border-active-grey-70 u-border-black u-border-hover-grey-70 u-border-no-left u-border-no-right u-border-no-top u-btn u-button-style u-hover-none u-none u-text-active-grey-70 u-text-black u-text-hover-grey-70 u-btn-1">find out more</a>
+                    </div>
+                </div>
+                <div class="u-align-center u-container-align-center u-container-style u-hover-feature u-list-item u-palette-1-light-2 u-repeater-item u-shape-rectangle u-list-item-2" data-animation-direction="Up" data-animation-name="customAnimationIn" data-animation-duration="1500">
+                    <div class="u-container-layout u-similar-container u-valign-top u-container-layout-3">
+                        <h3 class="u-align-center u-text u-text-body-color u-text-5"> Body</h3>
+                        <div class="u-border-5 u-border-white u-image u-image-circle u-image-2" alt="" data-image-width="920" data-image-height="920"></div>
+                        <p class="u-align-center u-text u-text-6"> Massage, Microdermabrassion, Exfoliation, Microneedling &amp; Hot Stone Therapy&nbsp; suscipit ultrices.</p> 
+                        <a href="https://nicepage.com/joomla-page-builder" class="u-active-none u-align-center u-border-1 u-border-active-grey-70 u-border-black u-border-hover-grey-70 u-border-no-left u-border-no-right u-border-no-top u-btn u-button-style u-hover-none u-none u-text-active-grey-70 u-text-black u-text-hover-grey-70 u-btn-2">find out more</a>
+                    </div>
+                </div>
+                <div class="u-align-center u-container-align-center u-container-style u-hover-feature u-list-item u-palette-1-light-2 u-repeater-item u-shape-rectangle u-list-item-3" data-animation-direction="Up" data-animation-name="customAnimationIn" data-animation-duration="1500">
+                    <div class="u-container-layout u-similar-container u-valign-top u-container-layout-4">
+                        <h3 class="u-align-center u-text u-text-body-color u-text-7">Nails</h3>
+                        <div class="u-border-5 u-border-white u-image u-image-circle u-image-3" alt="" data-image-width="710" data-image-height="710"></div>
+                        <p class="u-align-center u-text u-text-8"> Professional nail care treatments and products. Shellac and footlogic&nbsp; elit nullam nunc justo sagittis.</p> 
+                        <a href="https://nicepage.com/one-page-template" class="u-active-none u-align-center u-border-1 u-border-active-grey-70 u-border-black u-border-hover-grey-70 u-border-no-left u-border-no-right u-border-no-top u-btn u-button-style u-hover-none u-none u-text-active-grey-70 u-text-black u-text-hover-grey-70 u-btn-3">find out more</a>
+                    </div>
+                </div>
+                <div class="u-align-center u-container-align-center u-container-style u-hover-feature u-list-item u-palette-1-light-2 u-repeater-item u-shape-rectangle u-list-item-4" data-animation-direction="Up" data-animation-name="customAnimationIn" data-animation-duration="1500" data-animation-delay="750">
+                    <div class="u-container-layout u-similar-container u-valign-top u-container-layout-5">
+                        <h3 class="u-align-center u-text u-text-body-color u-text-9"> Waxing</h3>
+                        <div class="u-border-5 u-border-white u-image u-image-circle u-image-4" alt="" data-image-width="710" data-image-height="710"></div>
+                        <p class="u-align-center u-text u-text-10"> Waxing including Hollywood’s &amp; Brazilians. Adipiscing elit nullam nunc justo sagittis suscipit ultrices.</p> 
+                        <a href="https://nicepage.com/k/clothing-store-website-templates" class="u-active-none u-align-center u-border-1 u-border-active-grey-70 u-border-black u-border-hover-grey-70 u-border-no-left u-border-no-right u-border-no-top u-btn u-button-style u-hover-none u-none u-text-active-grey-70 u-text-black u-text-hover-grey-70 u-btn-4">find out more</a>
+                    </div>
+                </div>
+                <div class="u-align-center u-container-align-center u-container-style u-hover-feature u-list-item u-palette-1-light-2 u-repeater-item u-shape-rectangle u-list-item-5" data-animation-direction="Up" data-animation-name="customAnimationIn" data-animation-duration="1500" data-animation-delay="750">
+                    <div class="u-container-layout u-similar-container u-valign-top u-container-layout-6">
+                        <h3 class="u-align-center u-text u-text-body-color u-text-11"> Eyes</h3>
+                        <div class="u-border-5 u-border-white u-image u-image-circle u-image-5" alt="" data-image-width="710" data-image-height="710"></div>
+                        <p class="u-align-center u-text u-text-12"> Treatments by the original eyelash extension expertsnullam nunc justo sagittis suscipit ultrices.</p> 
+                        <a href="https://nicepage.com/k/auto-mechanic-html-templates" class="u-active-none u-align-center u-border-1 u-border-active-grey-70 u-border-black u-border-hover-grey-70 u-border-no-left u-border-no-right u-border-no-top u-btn u-button-style u-hover-none u-none u-text-active-grey-70 u-text-black u-text-hover-grey-70 u-btn-5">find out more</a>
+                    </div>
+                </div>
+                <div class="u-align-center u-container-align-center u-container-style u-hover-feature u-list-item u-palette-1-light-2 u-repeater-item u-shape-rectangle u-list-item-6" data-animation-direction="Up" data-animation-name="customAnimationIn" data-animation-duration="1500" data-animation-delay="750">
+                    <div class="u-container-layout u-similar-container u-valign-top u-container-layout-7">
+                        <h3 class="u-align-center u-text u-text-body-color u-text-13"> Tanning&nbsp;<br> </h3>
+                        <div class="u-border-5 u-border-white u-image u-image-circle u-image-6" alt="" data-image-width="710" data-image-height="710"></div>
+                        <p class="u-align-center u-text u-text-14"> Natural, vegan and cruelty-free tanning, consectetur adipiscing elit nullam nunc justo sagittis.</p> 
+                        <a href="https://nicepage.com/c/food-restaurant-website-templates" class="u-active-none u-align-center u-border-1 u-border-active-grey-70 u-border-black u-border-hover-grey-70 u-border-no-left u-border-no-right u-border-no-top u-btn u-button-style u-hover-none u-none u-text-active-grey-70 u-text-black u-text-hover-grey-70 u-btn-6">find out more</a>
+                    </div>
+                </div>
+                <div class="u-align-center u-container-align-center u-container-style u-hover-feature u-list-item u-palette-1-light-2 u-repeater-item u-shape-rectangle u-list-item-7" data-animation-direction="Up" data-animation-name="customAnimationIn" data-animation-duration="1500" data-animation-delay="1000">
+                    <div class="u-container-layout u-similar-container u-valign-top u-container-layout-8">
+                        <h3 class="u-align-center u-text u-text-body-color u-text-15"> Threading<br> </h3>
+                        <div class="u-border-5 u-border-white u-image u-image-circle u-image-7" alt="" data-image-width="710" data-image-height="710"></div>
+                        <p class="u-align-center u-text u-text-16"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt</p> 
+                        <a href="https://nicepage.com/html-website-builder" class="u-active-none u-align-center u-border-1 u-border-active-grey-70 u-border-black u-border-hover-grey-70 u-border-no-left u-border-no-right u-border-no-top u-btn u-button-style u-hover-none u-none u-text-active-grey-70 u-text-black u-text-hover-grey-70 u-btn-7">find out more</a>
+                    </div>
+                </div>
+                <div class="u-align-center u-container-align-center u-container-style u-hover-feature u-list-item u-palette-1-light-2 u-repeater-item u-shape-rectangle u-list-item-8" data-animation-direction="Up" data-animation-name="customAnimationIn" data-animation-duration="1500" data-animation-delay="1000">
+                    <div class="u-container-layout u-similar-container u-valign-top u-container-layout-9">
+                        <h3 class="u-align-center u-text u-text-body-color u-text-17"> Electrolysis<br> </h3>
+                        <div class="u-border-5 u-border-white u-image u-image-circle u-image-8" alt="" data-image-width="710" data-image-height="710"></div>
+                        <p class="u-align-center u-text u-text-18"> Includes a complimentary consultation, consectetur adipiscing elit nullam nunc justo sagittis.</p> 
+                        <a href="https://nicepage.com/c/table-html-templates" class="u-active-none u-align-center u-border-1 u-border-active-grey-70 u-border-black u-border-hover-grey-70 u-border-no-left u-border-no-right u-border-no-top u-btn u-button-style u-hover-none u-none u-text-active-grey-70 u-text-black u-text-hover-grey-70 u-btn-8">find out more</a>
+                    </div>
+                </div>
+                <div class="u-align-center u-container-align-center u-container-style u-hover-feature u-list-item u-palette-1-light-2 u-repeater-item u-shape-rectangle u-list-item-9" data-animation-direction="Up" data-animation-name="customAnimationIn" data-animation-duration="1500" data-animation-delay="1000">
+                    <div class="u-container-layout u-similar-container u-valign-top u-container-layout-10">
+                        <h3 class="u-align-center u-text u-text-body-color u-text-19">Pulsed Light<br> </h3>
+                        <div class="u-border-5 u-border-white u-image u-image-circle u-image-9" alt="" data-image-width="740" data-image-height="1110"></div>
+                        <p class="u-align-center u-text u-text-20"> For hair reduction, skin rejuvenation and acne, consectetur adipiscing elit nullam nunc justo sagittis.</p> 
+                        <a href="https://nicepage.com/css-templates" class="u-active-none u-align-center u-border-1 u-border-active-grey-70 u-border-black u-border-hover-grey-70 u-border-no-left u-border-no-right u-border-no-top u-btn u-button-style u-hover-none u-none u-text-active-grey-70 u-text-black u-text-hover-grey-70 u-btn-9">find out more</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Our Gallery Section Starts Here -->
+
+    <section class="u-align-center u-clearfix u-palette-1-light-2 u-section-2" id="carousel_0edc">
+        <div class="u-container-align-center u-container-style u-group u-white u-group-1">
+            <div class="u-container-layout u-valign-top u-container-layout-1">
+                <h2 class="u-align-center u-text u-text-default-lg u-text-default-md u-text-default-sm u-text-default-xl u-text-1" data-animation-name="customAnimationIn" data-animation-duration="1500" data-animation-delay="500">Our Gallery</h2>
+            </div>
+        </div>
+        <div class="u-gallery u-layout-grid u-lightbox u-show-text-on-hover u-gallery-1">
+            <div class="u-gallery-inner u-gallery-inner-1">
+                <div class="u-effect-fade u-effect-hover-zoom u-gallery-item u-shape-rectangle">
+                    <div class="u-back-slide" data-image-width="740" data-image-height="1022"> 
+                        <img class="u-back-image u-expanded" src="/src/assets/images/56767.jpg"> 
+                    </div>
+                    <div class="u-over-slide u-shading u-over-slide-1"></div>
+                </div>
+                <div class="u-effect-fade u-effect-hover-zoom u-gallery-item u-shape-rectangle">
+                    <div class="u-back-slide"> 
+                        <img class="u-back-image u-expanded" src="/src/assets/images/56677.jpg"> 
+                    </div>
+                    <div class="u-over-slide u-shading u-over-slide-2"></div>
+                </div>
+                <div class="u-effect-fade u-effect-hover-zoom u-gallery-item u-shape-rectangle" data-image-width="710" data-image-height="710">
+                    <div class="u-back-slide"> 
+                        <img class="u-back-image u-expanded" src="/src/assets/images/tyty.jpg"> 
+                    </div>
+                    <div class="u-over-slide u-shading u-over-slide-3"></div>
+                </div>
+                <div class="u-effect-fade u-effect-hover-zoom u-gallery-item u-shape-rectangle" data-image-width="710" data-image-height="710">
+                    <div class="u-back-slide"> 
+                        <img class="u-back-image u-expanded" src="/src/assets/images/547y54r.jpg"> 
+                    </div>
+                    <div class="u-over-slide u-shading u-over-slide-4"></div>
+                </div>
+                <div class="u-effect-fade u-effect-hover-zoom u-gallery-item u-shape-rectangle" data-image-width="1980" data-image-height="1321">
+                    <div class="u-back-slide"> 
+                        <img class="u-back-image u-expanded" src="/src/assets/images/56645.jpg"> 
+                    </div>
+                    <div class="u-over-slide u-shading u-over-slide-5"></div>
+                </div>
+                <div class="u-effect-fade u-effect-hover-zoom u-gallery-item u-shape-rectangle" data-image-width="740" data-image-height="1110">
+                    <div class="u-back-slide"> 
+                        <img class="u-back-image u-expanded" src="/src/assets/images/uyu.jpg"> 
+                    </div>
+                    <div class="u-over-slide u-shading u-over-slide-6"></div>
+                </div>
+                <div class="u-effect-fade u-effect-hover-zoom u-gallery-item u-shape-rectangle" data-image-width="740" data-image-height="1110">
+                    <div class="u-back-slide"> 
+                        <img class="u-back-image u-expanded" src="/src/assets/images/56677.jpg"> 
+                    </div>
+                    <div class="u-over-slide u-shading u-over-slide-7"></div>
+                </div>
+                <div class="u-effect-fade u-effect-hover-zoom u-gallery-item u-shape-rectangle" data-image-width="740" data-image-height="1110">
+                    <div class="u-back-slide"> 
+                        <img class="u-back-image u-expanded" src="/src/assets/images/46456.jpg"> 
+                    </div>
+                    <div class="u-over-slide u-shading u-over-slide-8"></div>
+                </div>
+                <div class="u-effect-fade u-effect-hover-zoom u-gallery-item u-shape-rectangle" data-image-width="740" data-image-height="1110">
+                    <div class="u-back-slide" data-image-width="1380" data-image-height="920"> 
+                        <img class="u-back-image u-expanded" src="/src/assets/images/45345.jpg"> 
+                    </div>
+                    <div class="u-over-slide u-shading u-over-slide-9"></div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style scoped>
+@import "../assets/css/Services.css"
+</style>
