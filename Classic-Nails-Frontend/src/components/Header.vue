7e9eea1f6ad8397c80a5a7b5552db78ce66ee51f
Trent <92242968+MrKingst3r@users.noreply.github.com>
2024-03-22 02:04:50 -0500

    Update Header.vue
    fixed header buttons to display correct color when activated.

--- Classic-Nails-Frontend/src/components/Header.vue
+++ Classic-Nails-Frontend/src/components/Header.vue
@@ -6,13 +6,13 @@
             <div class="u-custom-menu u-nav-container">
                 <ul class="u-nav u-spacing-2 u-unstyled u-nav-1">
                     <li class="u-nav-item">
-                        <router-link class="u-active-palette-4-base u-border-active-palette-1-base u-border-hover-palette-1-light-1 u-border-no-left u-border-no-right u-border-no-top u-button-style u-hover-palette-4-base u-nav-link u-text-active-grey-90 u-text-grey-90 u-text-hover-grey-90" to="/" style="padding: 10px 20px;">Home</router-link>
+                        <router-link :class="{ 'u-active-palette-4-base': $route.path === '/', 'u-border-active-palette-1-base u-border-hover-palette-1-light-1 u-border-no-left u-border-no-right u-border-no-top u-button-style u-hover-palette-4-base u-nav-link u-text-active-grey-90 u-text-grey-90 u-text-hover-grey-90': true }" to="/" style="padding: 10px 20px;">Home</router-link>
                     </li>
                     <li class="u-nav-item">
-                        <router-link class="u-active-palette-4-base u-border-active-palette-1-base u-border-hover-palette-1-light-1 u-border-no-left u-border-no-right u-border-no-top u-button-style u-hover-palette-4-base u-nav-link u-text-active-grey-90 u-text-grey-90 u-text-hover-grey-90" to="/services" style="padding: 10px 20px;">Services</router-link>
+                        <router-link :class="{ 'u-active-palette-4-base': $route.path === '/services', 'u-border-active-palette-1-base u-border-hover-palette-1-light-1 u-border-no-left u-border-no-right u-border-no-top u-button-style u-hover-palette-4-base u-nav-link u-text-active-grey-90 u-text-grey-90 u-text-hover-grey-90': true }" to="/services" style="padding: 10px 20px;">Services</router-link>
                     </li>
                     <li class="u-nav-item"> 
-                        <router-link class="u-active-palette-4-base u-border-active-palette-1-base u-border-hover-palette-1-light-1 u-border-no-left u-border-no-right u-border-no-top u-button-style u-hover-palette-4-base u-nav-link u-text-active-grey-90 u-text-grey-90 u-text-hover-grey-90" to="/login" style="padding: 10px 20px;">Login</router-link> 
+                        <router-link :class="{ 'u-active-palette-4-base': $route.path === '/login', 'u-border-active-palette-1-base u-border-hover-palette-1-light-1 u-border-no-left u-border-no-right u-border-no-top u-button-style u-hover-palette-4-base u-nav-link u-text-active-grey-90 u-text-grey-90 u-text-hover-grey-90': true }" to="/login" style="padding: 10px 20px;">Login</router-link> 
                     </li>
                 </ul>
             </div>
@@ -20,3 +20,10 @@
       </div>
   </header>
 </template>
+
+<style>
+.u-active-palette-4-base {
+     color: #111111 !important;
+     background-color: #f4e1cb !important;
+}
+</style>
